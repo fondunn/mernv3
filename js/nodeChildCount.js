6ddc3f16fa8a453933de84a47dfdbed4247b0351
Denis Karlinsky <64307212+fondunn@users.noreply.github.com>
2021-11-18 16:25:38 +0200

    Update nodeChildCount.js

--- js/nodeChildCount.js
+++ js/nodeChildCount.js
@@ -5,23 +5,16 @@ const span = document.createElement('span');
 p.appendChild(span);
 div.appendChild(p);
 
-function nodeChildCount(node, deep = 0) {
-    let nodes = [];
-    node.querySelectorAll("*").forEach(el => {
-        nodes.push(el);
-    })
-
-    while (deep > 0) {
-        let array = [];
-        nodes.forEach(e => {
-            for (let index = 0; index < e.children.length; index++) {
-                array.push(e.children[index]);
-            }
-        });
-        nodes = [...array];
-        deep--;
+function nodeChildCount(node, deep = null) {
+    let result = 0
+    const children = node.children;
+    if (deep === 0 || !children.length) {
+        return 0;
     }
-    return nodes.length;
+    for ( let child in el.children ) {
+        result += nodeChildCount(child, deep ? - 1 : null) + 1;
+    }
+    return result;
 }
 console.log(nodeChildCount(div))
 console.log(nodeChildCount(div, 1))
