5331cfb90344f5d679c6412740d17f7ab1277380
Denis Karlinsky <64307212+fondunn@users.noreply.github.com>
2021-11-18 16:24:28 +0200

    Update matrix.js

--- js/matrix.js
+++ js/matrix.js
@@ -1,46 +1,27 @@
-const baseMatrix = [
+const matrix = [
     [5, 3, 6], 
     [7, 11, 2],
     [15, 9, 4]
    ]
 
-const testMatrix = [[2, 2],1, [2,2], 3, [2,2]]
-
-function getSmallestNumber(matrix) {
-    let array = matrix.flat();
-    return Math.min.apply(null, array);
+function minNumber(matrix) {
+    return Math.min(...matrix.flat());
 }
 
-function isMatrix(matrix) {
-    let result = true;
-    matrix.map(el => {
-        if ( typeof el !== 'object') {
-            result = false
-        }
-    })
-    return result
-}
-
-function multiplyOdd(inputMatrix) {
-    const matrix = inputMatrix;
-    const smallestNumber = getSmallestNumber(inputMatrix)
-    const correctMatrix = isMatrix(inputMatrix)
-
+function multiplyOdd(matrix) {
+    
+    const smallestNumber = minNumber(matrix)
     let result = new Array;
 
-    if( correctMatrix === false ) {
-        return new Error('Not a matrix');
-    } else {
-        for ( array in matrix ) {
-            let idOdd = matrix[array].map(num => {
-                if ( num % 2 !== 0 ) {
-                    return num * smallestNumber;
-                } else return num;
-            })
-            result.push(idOdd);
-        }
+    for ( array in matrix ) {
+        let idOdd = matrix[array].map(num => {
+            if ( num % 2 !== 0 ) {
+                return num * smallestNumber;
+            } else return num;
+        })
+        result.push(idOdd);
     }
     return result
 }
 
-console.log(multiplyOdd(baseMatrix))
+console.log(multiplyOdd(matrix))
