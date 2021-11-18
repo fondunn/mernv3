19ab62e4d9d3ea34c6b37b2fc5238ceb683ac1ba
Denis Karlinsky <64307212+fondunn@users.noreply.github.com>
2021-11-18 16:22:19 +0200

    Update chunkArray.js

--- js/chunkArray.js
+++ js/chunkArray.js
@@ -1,33 +1,12 @@
-
-
-function chunkArray(data, split) {
-    const arr = [];
-
-    for ( let index = 0; index < data.length; index += split) {
-        arr.push(data.slice(index, index + split));
-    }
-
-    return {
-        next: () => {
-            const slice = arr.length > 0
-            ? arr.splice(0, 1)
-            : undefined;
-
-            return {
-                value: slice,
-                done: !slice
-            }
-        }
-    }
+function* chunkArray(data, piece) {
+    for ( let i = 0; i < data.length; i +=piece ) {
+        const chunk = data.slice(i, i + piece);
+        yield chunk;
+    }    
 }
+const iterator = chunkArray([1,2,3,4,5,6,7,8], 3);
 
-const iterator = chunkArray([1,2,3,4,5,6,7,8], 2);
-
 console.log(iterator.next())
 console.log(iterator.next())
 console.log(iterator.next())
 console.log(iterator.next())
-console.log(iterator.next())
-// iterator.next()
-// iterator.next()
-// iterator.next()
